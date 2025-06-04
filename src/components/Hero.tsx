2e468adb796073c7c634ea7b28f66d02051ef14d
Moses Joseph <mosesejoseph@gmail.com>
2025-06-04 19:43:57 -0400

    Added Hero components

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 // @ts-expect-error testing
 const Hero = (props) => {
   return (
     <>
-    <div className='text-xl text-red'>{props.title}</div>
-    <div className='text-xl text-red'>{props.subtitle}</div>
+      <div className="text-xl text-red">{props.title}</div>
+      <div className="text-xl text-red">{props.subtitle}</div>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
